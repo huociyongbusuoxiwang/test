13eb7466e37d9b24d8e45c7bff43bd120dd41970
小豆泥的编程之路 <148734780+huociyongbusuoxiwang@users.noreply.github.com>
2025-04-16 20:50:58 +0800

    更改

--- src/test/test.java
+++ src/test/test.java
@@ -3,5 +3,6 @@ package test;
 public class test {
     public static void main(String[] args) {
         Student student = new Student("豆泥", "女", 8);
+        System.out.println("student = " + student);
     }
 }
